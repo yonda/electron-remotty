d9c66121cd93c0240d43e2c8abaf1bee6262a900
yonda <honda@33i.co.jp>
2015-09-29 21:12:10 +0900

    modify window size

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.on('window-all-closed', function () {
 
 app.on('ready', function () {
   // ブラウザ(Chromium)の起動, 初期画面のロード
-  mainWindow = new BrowserWindow({width: 1000, height: 600});
+  mainWindow = new BrowserWindow({width: 1024, height: 600});
 
   mainWindow.loadUrl('file://' + __dirname + '/index.html');
   mainWindow.on('closed', function () {
