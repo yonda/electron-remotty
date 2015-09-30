001c48b7086cf886b7e175e6fd1e5c16c1289a82
HaiTo <kimura@sansan.com>
2015-09-30 11:22:06 +0900

    cannnable application menus

--- index.js
+++ index.js
@@ -3,8 +3,6 @@
 var app = require('app');
 var BrowserWindow = require('browser-window');
 
-require('menu');
-
 require('crash-reporter').start();
 
 var mainWindow = null;
@@ -20,7 +18,38 @@ app.on('ready', function () {
   mainWindow = new BrowserWindow({width: 1280, height: 800});
 
   mainWindow.loadUrl('file://' + __dirname + '/index.html');
+
+  setApplicationMenu();
+
   mainWindow.on('closed', function () {
     mainWindow = null;
   });
 });
+
+function setApplicationMenu (){
+  var Menu = require('menu');
+
+  var menu = Menu.buildFromTemplate([
+    {
+      label: "Application",
+      submenu: [
+        {label: "About Application", selector: "orderFrontStandardAboutPanel:"},
+        {type: "separator"},
+        {label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+      ]
+    },
+    {
+      label: "Edit",
+      submenu: [
+        { label: "Undo", accelerator: "Command+Z", selector: "undo:" },
+        { label: "Redo", accelerator: "Shift+Command+Z", selector: "redo:" },
+        { type: "separator" },
+        { label: "Cut", accelerator: "Command+X", selector: "cut:" },
+        { label: "Copy", accelerator: "Command+C", selector: "copy:" },
+        { label: "Paste", accelerator: "Command+V", selector: "paste:" },
+        { label: "Select All", accelerator: "Command+A", selector: "selectAll:" }
+      ]
+    }
+  ]);
+  Menu.setApplicationMenu(menu);
+}
